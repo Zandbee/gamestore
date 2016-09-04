b12f4dc2c9c3fb3978876394636fde1db81c52b9
Veronika Strokova <vesna.adrs@gmail.com>
2016-09-04 22:18:01 +0300

    init

---
+++ src/main/java/org/strokova/gamestore/config/ConfigMySql.java
@@ -0,0 +1,34 @@
+package org.strokova.gamestore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * author: Veronika, 9/4/2016.
+ */
+
+@Configuration
+@ComponentScan
+@PropertySource("datasource.properties")
+public class ConfigMySql {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("ds.driver"));
+        dataSource.setUrl(env.getProperty("ds.url"));
+        dataSource.setUsername(env.getProperty("ds.username"));
+        dataSource.setPassword(env.getProperty("ds.password"));
+
+        return dataSource;
+    }
+}
