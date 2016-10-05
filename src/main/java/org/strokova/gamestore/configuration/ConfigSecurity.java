7eea2f5b2b314a0ada7cf6ca38e2c500436a09c3
Veronika Strokova <veronika.strokova@dataart.com>
2016-10-05 15:37:45 +0300

    configure access with Spring Security. show message when there are no apps in a category

--- src/main/java/org/strokova/gamestore/configuration/ConfigSecurity.java
+++ src/main/java/org/strokova/gamestore/configuration/ConfigSecurity.java
@@ -3,11 +3,14 @@ package org.strokova.gamestore.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.strokova.gamestore.model.Role;
 
 import javax.sql.DataSource;
 
@@ -27,10 +30,22 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource)
-        .usersByUsernameQuery(USERS_BY_USERNAME_QUERY)
-        .authoritiesByUsernameQuery(AUTHORITIES_BY_USERNAME_QUERY)
-        .passwordEncoder(new BCryptPasswordEncoder()); // TODO: use passwordEncoder() instead of new?
+                .usersByUsernameQuery(USERS_BY_USERNAME_QUERY)
+                .authoritiesByUsernameQuery(AUTHORITIES_BY_USERNAME_QUERY)
+                .passwordEncoder(new BCryptPasswordEncoder()); // TODO: use passwordEncoder() instead of new?
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .and().authorizeRequests()
+                    .antMatchers("/registration", "/login").permitAll()
+                    .antMatchers("/upload/**").hasRole(Role.DEVELOPER.toString()) //TODO: use name() instead of toString()?
+                    .anyRequest().authenticated();
+                //.and().requiresChannel()
+                    //.antMatchers("/registration", "/login").requiresSecure(); // send user's credentials over HTTPS
     }
 
     @Bean
