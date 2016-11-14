9e3d0bc88268aa682cf9e7c9a83dcebb85e3abce
Veronika Strokova <veronika.strokova@dataart.com>
2016-11-14 13:27:33 +0300

    read path in webappinitializer from properties

--- src/main/java/org/strokova/gamestore/configuration/WebAppInitializer.java
+++ src/main/java/org/strokova/gamestore/configuration/WebAppInitializer.java
@@ -10,11 +10,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 
 /**
  * @author vstrokova, 06.09.2016.
  */
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    private static Logger logger = Logger.getLogger(WebAppInitializer.class.getName());
 
     private static final int MAX_FILE_SIZE = 2097152; // 2 MB
     private static final int MAX_REQUEST_SIZE = 4194304; // 4 MB
@@ -44,6 +47,7 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected void customizeRegistration(ServletRegistration.Dynamic registration) {
         try {
+            logger.info("Folder path in WebAppInitializer = " + path);
             Path multipartUploadsPath = Paths.get(System.getProperty(DEFAULT_TEMP_DIR_PROPERTY) + MULTIPART_TEMP_DIR_NAME);
             if (Files.notExists(multipartUploadsPath)) {
                 Files.createDirectories(multipartUploadsPath);
